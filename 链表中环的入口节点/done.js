819a602484ef251bd66dbd17899c1ed38abeb542
张明朝 <1104272319@qq.com>
2018-06-01 20:51:21 +0800

    加注释

--- 链表中环的入口节点/done.js
+++ 链表中环的入口节点/done.js
@@ -10,12 +10,13 @@ function EntryNodeOfLoop(pHead)
 
     let p1 = pHead;
     let p2 = pHead;
-
+    
+    //很巧妙，null是没有next的，否则oj会报访问越界错误，判断p2可走再p2.next可否走即可
     while(p2 !==null && p2.next !== null) {
       p1 = p1.next;
       p2 = p2.next.next;
       if(p1 === p2) {
-        p2 = pHead;
+        p1 = pHead;
         while(p1!==p2) {
           p1 = p1.next;
           p2 = p2.next;
